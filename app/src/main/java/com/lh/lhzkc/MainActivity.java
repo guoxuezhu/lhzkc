cb592b784db6e1749c1351862a05b9dde5eafb40
guoxuezhu <1174327885@qq.com>
2019-04-02 08:54:12 +0800

    fix 1

--- app/src/main/java/com/lh/lhzkc/MainActivity.java
+++ app/src/main/java/com/lh/lhzkc/MainActivity.java
@@ -1,9 +1,9 @@
 package com.lh.lhzkc;
 
-import android.support.v7.app.AppCompatActivity;
+import android.app.Activity;
 import android.os.Bundle;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends Activity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
