10454bd440bfdd78912579314422d8952887b573
guoxuezhu <1174327885@qq.com>
2019-04-05 22:38:57 +0800

    fix 1

--- app/src/main/java/com/lh/lhzkc/ui/DianyuanActivity.java
+++ app/src/main/java/com/lh/lhzkc/ui/DianyuanActivity.java
@@ -1,10 +1,13 @@
 package com.lh.lhzkc.ui;
 
 import android.app.Activity;
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
 import com.lh.lhzkc.R;
+import com.lh.lhzkc.utils.HttpUtil;
+
+import butterknife.ButterKnife;
+import butterknife.OnClick;
 
 public class DianyuanActivity extends Activity {
 
@@ -12,5 +15,20 @@ public class DianyuanActivity extends Activity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_dianyuan);
+
+        ButterKnife.bind(this);
+
     }
+
+
+    @OnClick(R.id.dy_all_open)
+    public void dy_all_open() {
+        HttpUtil.myPost("37");
+    }
+
+    @OnClick(R.id.dy_all_close)
+    public void dy_all_close() {
+        HttpUtil.myPost("38");
+    }
+
 }
--- app/src/main/java/com/lh/lhzkc/ui/LuboActivity.java
+++ app/src/main/java/com/lh/lhzkc/ui/LuboActivity.java
@@ -5,6 +5,10 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
 import com.lh.lhzkc.R;
+import com.lh.lhzkc.utils.HttpUtil;
+
+import butterknife.ButterKnife;
+import butterknife.OnClick;
 
 public class LuboActivity extends Activity {
 
@@ -12,5 +16,44 @@ public class LuboActivity extends Activity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_lubo);
+
+        ButterKnife.bind(this);
+
+
     }
+
+
+
+    @OnClick(R.id.luzhi)
+    public void luzhi() {
+        HttpUtil.myPost("33");
+    }
+
+    @OnClick(R.id.zanting)
+    public void zanting() {
+        HttpUtil.myPost("34");
+    }
+
+    @OnClick(R.id.tingzhi)
+    public void tingzhi() {
+        HttpUtil.myPost("35");
+    }
+
+    @OnClick(R.id.zhibo)
+    public void zhibo() {
+        HttpUtil.myPost("36");
+    }
+
+
+
+
+
+
+
+
+
+
+
+
+
 }
