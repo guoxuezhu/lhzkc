63e34457dd875a867cc3b3af7dc14419510468be
guoxuezhu <1174327885@qq.com>
2019-04-06 23:20:33 +0800

    fix UI

--- app/src/main/java/com/lh/lhzkc/ui/HomeActivity.java
+++ app/src/main/java/com/lh/lhzkc/ui/HomeActivity.java
@@ -24,67 +24,43 @@ public class HomeActivity extends Activity {
     @OnClick(R.id.cj_btn_layout)
     public void cj_btn_layout() {
         startActivity(new Intent(this, ChangjingActivity.class));
-        finish();
     }
 
 
     @OnClick(R.id.sb_btn_layout)
     public void sb_btn_layout() {
         startActivity(new Intent(this, ShebeiActivity.class));
-        finish();
     }
 
 
     @OnClick(R.id.lb_btn_layout)
     public void lb_btn_layout() {
         startActivity(new Intent(this, LuboActivity.class));
-        finish();
     }
 
 
     @OnClick(R.id.jz_btn_layout)
     public void jz_btn_layout() {
         startActivity(new Intent(this, JuzhenActivity.class));
-        finish();
     }
 
 
     @OnClick(R.id.hj_btn_layout)
     public void hj_btn_layout() {
         startActivity(new Intent(this, HuanjingActivity.class));
-        finish();
     }
 
 
     @OnClick(R.id.yp_btn_layout)
     public void yp_btn_layout() {
         startActivity(new Intent(this, YinpinActivity.class));
-        finish();
     }
 
 
     @OnClick(R.id.dy_btn_layout)
     public void dy_btn_layout() {
         startActivity(new Intent(this, DianyuanActivity.class));
-        finish();
     }
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
 
 
 }
